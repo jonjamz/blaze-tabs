a284bfb6d82326c06f5701b32c1c871a1400eec3
Jonathan James <jonjamz86@gmail.com>
2015-02-06 14:41:36 -0500

    update to 2.1.5

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'templates:tabs',
   summary: 'Reactive tabbed interfaces compatible with routing.',
-  version: '2.1.4',
+  version: '2.1.5',
   git: 'https://github.com/meteortemplates/tabs.git'
 });
 
